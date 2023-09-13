338d659ae07cc8ff9b69cee0e380e2df0c6db3d5
pkandic4 <petarkandic85@gmail.com>
2023-09-13 16:22:33 +0000

    removed insert statements

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -13,12 +13,6 @@ CREATE TABLE IF NOT EXISTS  PUBLIC.host_info(
      CONSTRAINT host_info_pk PRIMARY KEY (id), 
      CONSTRAINT host_info_un UNIQUE (hostname));
 
-INSERT INTO host_info (id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, "timestamp", total_mem) VALUES(1, 'jrvs-remote-desktop-centos7-6.us-central1-a.c.spry-framework-236416.internal', 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300, 256, '2019-05-29 17:49:53.000', 601324);
-
-INSERT INTO host_info (id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, "timestamp", total_mem) VALUES(2, 'noe1', 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300, 256, '2019-05-29 17:49:53.000', 601324);
-
-INSERT INTO host_info (id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, "timestamp", total_mem) VALUES(3, 'noe2', 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300, 256, '2019-05-29 17:49:53.000', 601324);
-
 CREATE TABLE IF NOT EXISTS PUBLIC.host_usage 
   ( 
      "timestamp"    TIMESTAMP NOT NULL, 
@@ -32,9 +26,4 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
      host_info(id) 
   );
 
-INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) 
-VALUES('2019-05-29 15:00:00.000', 1, 300000, 90, 4, 2, 3);
-
-INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) 
-VALUES('2019-05-29 15:01:00.000', 1, 200000, 90, 4, 2, 3);
 
