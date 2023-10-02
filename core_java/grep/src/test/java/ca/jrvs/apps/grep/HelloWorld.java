2e5ba1081305eb8ee815ab164a573ceac53a6d2b
pkandic4 <petarkandic85@gmail.com>
2023-10-02 15:29:06 +0000

    added HelloWorld

--- core_java/grep/src/test/java/ca/jrvs/apps/grep/HelloWorld.java
+++ core_java/grep/src/test/java/ca/jrvs/apps/grep/HelloWorld.java
@@ -1,9 +1,10 @@
 package ca.jrvs.apps.practice;
 
-class HelloWorld {
+class HelloWorld 
+{
 
        public static void main(String args[]) 
-{
+       {
            System.out.println("Hello, World");
-           }       
+       }       
 }    
