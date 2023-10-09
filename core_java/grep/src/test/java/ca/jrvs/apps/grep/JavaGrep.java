55fd8fb417ca35ccdc072672f40a926b66b267f2
pkandic4 <45543578+pkandic4@users.noreply.github.com>
2023-10-09 14:16:30 -0400

    Update JavaGrep.java

--- core_java/grep/src/test/java/ca/jrvs/apps/grep/JavaGrep.java
+++ core_java/grep/src/test/java/ca/jrvs/apps/grep/JavaGrep.java
@@ -6,44 +6,14 @@ import java.util.List;
 
 public interface JavaGrep
 {
-    /**
- *      * Top level search workflow
- *           * @throws IOException
- *                */
     void process() throws IOException;
 
-    /**
- *      * Traverse a given directory and return all files
- *           * @param rootDir input directory
- *                * @return files under the rootDir
- *                     */
     List<File> listFiles(String rootDir);
 
-    /**
- *      * Read a file, and return all the lines
- *           *
- *                * Explain FileReader, BufferedReader, and character encoding
- *                     *
- *                          * @param inputFile file to be read
- *                               * @return lines
- *                                    */
     List<String> readLines(File inputFile);
 
-    /**
- *      * Check if a line contains the regex pattern (passed by a user)
- *           * @param line input string
- *                * @return true if there is a match
- *                     */
     boolean containsPattern(String line);
 
-    /**
- *      * Write lines to a file
- *           *
- *                * Explore: FileOutputStream, OutputStreamWriter, and BufferedStreamWriter
- *                     *
- *                          * @param lines matched line
- *                               * @throws IOException if write failed
- *                                    */
     void writeToFile(List<String> lines) throws IOException;
 
     String getRootPath();
