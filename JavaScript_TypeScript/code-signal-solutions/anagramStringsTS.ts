c6f474ffb39388338c21ae08711d28e17db7339d
pkandic4 <45543578+pkandic4@users.noreply.github.com>
2023-11-19 11:14:40 -0500

    Add files via upload

--- JavaScript_TypeScript/code-signal-solutions/anagramStringsTS.ts
+++ JavaScript_TypeScript/code-signal-solutions/anagramStringsTS.ts
@@ -1,1 +1,9 @@
+let stringOne = "whom";
+let stringTwo = "whom";
+console.log(anagramString(stringOne, stringTwo));
 
+function anagramString(strOne, strTwo)
+{
+    const norm = str =>  str.toLowerCase().replace(/[^a-z\d]/g, '').split('').sort().join('');
+    return norm(strOne) === norm(strTwo);
+}
